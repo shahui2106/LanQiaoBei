b94c362651b45fd16f9ec651e35d9e5aa1459049
Zhu <zhuzhu@123.com>
2020-05-03 20:52:05 +0800

    每月的天数

---
+++ src/SuanFaXunLian/MeiYuedeTianShu/Main.java
@@ -0,0 +1,36 @@
+package SuanFaXunLian.MeiYuedeTianShu;
+
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * 算法训练 -- 每月的天数
+ *
+ * @author Zhu
+ * @createtime 2020/5/2-15:35
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println(31);
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println(30);
+                break;
+            case 2:
+                System.out.println(28);
+        }
+    }
+}
