be23df18aef28a0e27d8c7ec91029f34d2fe1acc
Zhu <zhuzhu@123.com>
2020-06-28 19:26:27 +0800

    [ACM]初学系列（7）a+b

---
+++ src/Nuist_OJ/P1090ACM初学系列7a加b/Main.java
@@ -0,0 +1,19 @@
+package Nuist_OJ.P1090ACM初学系列7a加b;
+
+import java.util.Scanner;
+
+/**
+ * OJ系统练习题 -- [ACM]初学系列（7）a+b
+ *
+ * @author Zhu
+ * @createtime 2020/6/28-16:39
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.nextInt() + sc.nextInt());
+            System.out.println();
+        }
+    }
+}
